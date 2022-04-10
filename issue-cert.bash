c658e1f06b01987e526b6e093ac2f26d502f3090
Dmitry Savonin <dmitry@ankr.com>
2022-04-10 16:28:56 +0300

    issue certs only if they are not provided yet

--- issue-cert.bash
+++ issue-cert.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-~/.acme.sh/acme.sh --issue --standalone \
+[ ! -d "/root/.acme.sh/rpc.${DOMAIN_NAME}" ] && ~/.acme.sh/acme.sh --issue --standalone \
   -d rpc.${DOMAIN_NAME} \
   -d explorer.${DOMAIN_NAME} \
   -d staking.${DOMAIN_NAME} \
