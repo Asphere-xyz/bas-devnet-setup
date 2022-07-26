e9bfd2f3bc82e9db20ca2777c5081d81400b85ed
Dmitry Savonin <dmitry@ankr.com>
2022-07-26 18:45:19 +0300

    fixed compose file and issue cert script

--- issue-cert.bash
+++ issue-cert.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 [ ! -d "/root/.acme.sh/rpc.${DOMAIN_NAME}" ] && ~/.acme.sh/acme.sh --issue --standalone \
-  -d blockscout.${DOMAIN_NAME} \
   -d rpc.${DOMAIN_NAME} \
+  -d blockscout.${DOMAIN_NAME} \
   -d explorer.${DOMAIN_NAME} \
   -d staking.${DOMAIN_NAME} \
   -d faucet.${DOMAIN_NAME} || true
